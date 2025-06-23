ee7b4167a27f033d915a4f2b2c8cb6e9bd47772a
Unknown <87952662+bbksu@users.noreply.github.com>
2025-06-23 10:23:19 +0000

    Create main.py
    Array and set

---
+++ Math/set/main.py
@@ -0,0 +1,18 @@
+# _ = input(" ")
+# t_array = input(" ")
+# a = input(" ")
+# b = input(" ")
+
+t_array = "1 5 3"
+a = "3 1"
+b = "5 7"
+
+t_array = list(t_array)
+a = set(a)
+b = set(b)
+# l_a = len(a & t_array)
+# l_b = len(b & t_array)
+# print(l_a - l_b)
+
+for i in t_array:
+    print(i)
