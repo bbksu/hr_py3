c18aa076cbfd6a27fc31d3fee5e53c03e3e05a79
Unknown <87952662+bbksu@users.noreply.github.com>
2025-06-28 15:35:51 +0000

    Create main.py
    student score average

---
+++ data_structures/ds3/main.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+
+    student = student_marks[query_name]
+    print("%.2f" %(sum(student) / len(student)))
