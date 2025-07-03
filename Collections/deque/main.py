92f73df6d7e80ffe00d9d25586155b60b3302072
Unknown <87952662+bbksu@users.noreply.github.com>
2025-07-03 15:54:59 +0000

    Update main.py
    collections.deque

--- Collections/deque/main.py
+++ Collections/deque/main.py
@@ -1,21 +1,11 @@
 from collections import deque
 
-#items = int(input())
-items = 6
-
-dat = ["append 1",
-"append 2",
-"append 3",
-"appendleft 4",
-"pop",
-"popleft"]
+items = int(input())
 
 de = deque()
-cls = globals()["de"]
-
 
 for i in range(items):    
-    func = dat[i].split()    
+    func = input().split()     
     if len(func) != 1:
         getattr(de, func[0],)(func[-1])
     else:        
