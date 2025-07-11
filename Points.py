4b7d272866babd08df1f3f61c1cdf4d854b39a5e
Unknown <87952662+bbksu@users.noreply.github.com>
2025-07-11 19:18:37 +0000

    Create Points.py

---
+++ Points.py
@@ -0,0 +1,44 @@
+import math
+
+# 0 4 5
+# 1 7 6
+# 0 5 9
+# 1 7 2
+
+class Points(object):
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __sub__(self, no):
+        x = self.x - no.x
+        y = self.y - no.y
+        z = self.z - no.z
+        return Points(x, y, z)
+
+    def dot(self, no):
+        return sum(map(lambda a, b: a * b, *[[self.x, self.y, self.z], [no.x, no.y, no.z]]))
+
+    def cross(self, no):
+        i = (self.y * no.z - self.z * no.y)
+        j = (self.x * no.z - self.z * no.x)
+        k = (self.x * no.y - self.y * no.x)
+        return Points(i, j, k)
+
+    def absolute(self):
+        return pow((self.x ** 2 + self.y ** 2 + self.z ** 2), 0.5)
+
+
+if __name__ == '__main__':
+    points = list()
+    for i in range(4):
+        a = list(map(float, input().split()))
+        points.append(a)
+
+    a, b, c, d = Points(*points[0]), Points(*points[1]), Points(*points[2]), Points(*points[3])
+    x = (b - a).cross(c - b)
+    y = (c - b).cross(d - c)
+    angle = math.acos(x.dot(y) / (x.absolute() * y.absolute()))
+
+    print("%.2f" % math.degrees(angle))
