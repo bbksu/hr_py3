12a806f68cfbacaf110dc07ecaf7435f70c0d682
Unknown <87952662+bbksu@users.noreply.github.com>
2025-07-27 12:39:02 +0000

    Update main.py
    fix

--- Strings/string_formatting/main.py
+++ Strings/string_formatting/main.py
@@ -2,9 +2,9 @@ def print_formatted(number):
     size = len(bin(number)[2:])
     for i in range(1, number + 1):
         i = int(i)
-        print(f"{i:>{size}} {oct(i)[2:]:>{size}} {hex(i).upper()[2:]:>{size}} {bin(i)[2:]:>{size}}")
+        print(f"{i:>{size}} {oct(i)[2:]:>{size}} {hex(i)[2:]:>{size}} {bin(i)[2:]:>{size}}".upper())
 
 
 if __name__ == '__main__':
-    n = int(input())    
+    n = int(input())
     print_formatted(n)
