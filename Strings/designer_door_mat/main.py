6f9926c57f385436a15c31861928d4091086a59c
Unknown <87952662+bbksu@users.noreply.github.com>
2025-07-27 15:37:04 +0000

    Update main.py
    cleanup

--- Strings/designer_door_mat/main.py
+++ Strings/designer_door_mat/main.py
@@ -2,26 +2,15 @@ def door_mat(height):
     height = int(height)
     width = height * 3
     midpoint = (height + 1) // 2
-    mid = ".|."
-    i = 0
-    rev = False
-    while True:
-        center = f"{mid * i}{mid}{mid * i}"
-        if i + 1 == midpoint:
-            rev = True
-            center = "WELCOME"
-        size = width - len(center)
-        dash = "-" * (size // 2)
-        shape = f"{dash}{center}{dash}"
-        print(shape)
-        if rev:
-            i -= 1
-        else:
-            i += 1        
-        if i < 0:
-            break
-
+    mid_string = ".|."
+    for i in range(0, height):        
+        i = min(height - (i+1), i)
+        center = f"{mid_string * i}{mid_string}{mid_string * i}"
+        if i + 1 == midpoint:            
+            center = "WELCOME"        
+        dash = "-" * ((width - len(center)) // 2)        
+        print(f"{dash}{center}{dash}")
 
 if __name__ == '__main__':
     N, M = input().split()
-    door_mat(N)   
+    door_mat(N)       
