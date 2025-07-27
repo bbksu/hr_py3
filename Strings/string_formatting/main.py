90585b97223ee71ed66799264981f720ba99c0bc
Unknown <87952662+bbksu@users.noreply.github.com>
2025-07-27 12:34:15 +0000

    Update main.py
    cleanup

--- Strings/string_formatting/main.py
+++ Strings/string_formatting/main.py
@@ -1,23 +1,10 @@
 def print_formatted(number):
-    # your code goes here
-    n_string = ""
-    m_ax = bin(number)[2:]
-    size = len(m_ax)
+    size = len(bin(number)[2:])
     for i in range(1, number + 1):
-        i = int(i)                
-        g = f"{i:>{size}} {oct(i)[2:]:>{size}} {hex(i).upper()[2:]:>{size}} {bin(i)[2:]:>{size}}"
+        i = int(i)
+        print(f"{i:>{size}} {oct(i)[2:]:>{size}} {hex(i).upper()[2:]:>{size}} {bin(i)[2:]:>{size}}")
 
-        # bins = f"{bin(i)[2:]:>5}"        
-        # hexes = f"{hex(i)[2:]:>{size}}"               
-        # print(hexes)
-        print(g)
-        # print(bins)
-        # n_string += g.upper()
-    # print(n_string[:-1])
-
-    
 
 if __name__ == '__main__':
-    # n = int(input())
-    n = 17
+    n = int(input())    
     print_formatted(n)
