8b9cd4d979d9e2fd53e763ae49679c5cdb410299
Unknown <87952662+bbksu@users.noreply.github.com>
2025-07-27 17:59:07 +0000

    Update main.py

--- Strings/designer_door_mat/main.py
+++ Strings/designer_door_mat/main.py
@@ -3,14 +3,15 @@ def door_mat(height):
     width = height * 3
     midpoint = (height + 1) // 2
     mid_string = ".|."
-    for i in range(0, height):        
-        i = min(height - (i+1), i)
+    for i in range(0, height):
+        i = min(height - (i + 1), i)
         center = f"{mid_string * i}{mid_string}{mid_string * i}"
-        if i + 1 == midpoint:            
-            center = "WELCOME"        
-        dash = "-" * ((width - len(center)) // 2)        
+        if i + 1 == midpoint:
+            center = "WELCOME"
+        dash = "-" * ((width - len(center)) // 2)
         print(f"{dash}{center}{dash}")
+
 
 if __name__ == '__main__':
     N, M = input().split()
-    door_mat(N)       
+    door_mat(N)
