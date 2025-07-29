70fe7d521b3156cb53ded4ac22526759cdd111f3
Unknown <87952662+bbksu@users.noreply.github.com>
2025-07-29 00:26:33 +0000

    Update main.py

--- Strings/alphabet_rangoli/main.py
+++ Strings/alphabet_rangoli/main.py
@@ -1,21 +1,21 @@
 def print_rangoli(size):
     # your code goes here
-    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]    
-    
-    
-    length = (size - 1)*2
-    length = length + length+1
-    print(length)
-    
-    letters = "-".join(alphabet[:size])
-    print(letters)
-    # for i in range(size):
-    #     letters += f"{alphabet[i]}-"
-    #     print(length*"-")    
+    alphabet = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
+                "v", "w", "x", "y", "z")
+    length = (size - 1) * 2
+    length = length + length + 1
 
-    
-    
+    dashes = length // 2 * "-"
+    rangoli = []
+    for i in range(size):
+        row = "-".join(alphabet[(size - 1) - i:size])
+        dash = len(dashes) - len(row) + 1
+        row = f"{row}{dash * '-'}"
+        rangoli.append((row[:0:-1] + row))
+    rangoli = rangoli + rangoli[:-1][::-1]
+    print("\n".join(rangoli))
+
+
 if __name__ == '__main__':
-    # n = int(input())
-    n = 3
+    n = int(input())
     print_rangoli(n)
